7a354727c4866e39c8ed8468bed9b448f4633e38
enzo <enzo.batrov@55pbx.com.br>
2019-10-25 19:57:04 -0300

    Implementaçao de socket para jogadores

--- server.js
+++ server.js
@@ -6,8 +6,25 @@ const Game = require('./Game');
 const game = new Game();
 
 io.on('connection', socket => {
-    game.addPlayer(socket.id);
+    const player = game.addPlayer(socket.id);
+
     socket.emit('build_game', game);
+
+    socket.broadcast.emit('add_player', {
+        player,
+        id: socket.id
+    });
+
+    socket.on('move_player', (direction) => {
+        const player = game.players[socket.id];
+        console.log(player)
+        player.move(direction);
+        console.log(player)
+        socket.broadcast.emit('player_moved', {
+            player,
+            id: socket.id
+        });
+    });
 });
 
 httpServer.listen(3000, () => {
